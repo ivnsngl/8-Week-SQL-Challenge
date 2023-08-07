9b111528c22d308637bb6d105384b2ad970b8e8d
Ivan Justine Sangel <58377374+ivnsngl@users.noreply.github.com>
2023-08-07 14:41:38 -0400

    Update A. Pizza Metrics.sql

--- 2 - Pizza Runner/A. Pizza Metrics.sql
+++ 2 - Pizza Runner/A. Pizza Metrics.sql
@@ -3,122 +3,122 @@ SET search_path = pizza_runner;
 
 DROP TABLE IF EXISTS runners;
 CREATE TABLE runners (
-  "runner_id" INTEGER,
-  "registration_date" DATE
+	"runner_id" INTEGER,
+	"registration_date" DATE
 );
 INSERT INTO runners
-  ("runner_id", "registration_date")
+	("runner_id", "registration_date")
 VALUES
-  (1, '2021-01-01'),
-  (2, '2021-01-03'),
-  (3, '2021-01-08'),
-  (4, '2021-01-15');
+	(1, '2021-01-01'),
+	(2, '2021-01-03'),
+	(3, '2021-01-08'),
+	(4, '2021-01-15');
 
 
 DROP TABLE IF EXISTS customer_orders;
 CREATE TABLE customer_orders (
-  "order_id" INTEGER,
-  "customer_id" INTEGER,
-  "pizza_id" INTEGER,
-  "exclusions" VARCHAR(4),
-  "extras" VARCHAR(4),
-  "order_time" TIMESTAMP
+	"order_id" INTEGER,
+	"customer_id" INTEGER,
+	"pizza_id" INTEGER,
+	"exclusions" VARCHAR(4),
+	"extras" VARCHAR(4),
+	"order_time" TIMESTAMP
 );
 
 INSERT INTO customer_orders
-  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+	("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
 VALUES
-  ('1', '101', '1', '0', '0', '2020-01-01 18:05:02'),
-  ('2', '101', '1', '0', '0', '2020-01-01 19:00:52'),
-  ('3', '102', '1', '0', '0', '2020-01-02 23:51:23'),
-  ('3', '102', '2', '0', '0', '2020-01-02 23:51:23'),
-  ('4', '103', '1', '4', '0', '2020-01-04 13:23:46'),
-  ('4', '103', '1', '4', '0', '2020-01-04 13:23:46'),
-  ('4', '103', '2', '4', '0', '2020-01-04 13:23:46'),
-  ('5', '104', '1', '0', '1', '2020-01-08 21:00:29'),
-  ('6', '101', '2', '0', '0', '2020-01-08 21:03:13'),
-  ('7', '105', '2', '0', '1', '2020-01-08 21:20:29'),
-  ('8', '102', '1', '0', '0', '2020-01-09 23:54:33'),
-  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
-  ('10', '104', '1', '0', '0', '2020-01-11 18:34:49'),
-  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+	('1', '101', '1', '0', '0', '2020-01-01 18:05:02'),
+	('2', '101', '1', '0', '0', '2020-01-01 19:00:52'),
+	('3', '102', '1', '0', '0', '2020-01-02 23:51:23'),
+	('3', '102', '2', '0', '0', '2020-01-02 23:51:23'),
+	('4', '103', '1', '4', '0', '2020-01-04 13:23:46'),
+	('4', '103', '1', '4', '0', '2020-01-04 13:23:46'),
+	('4', '103', '2', '4', '0', '2020-01-04 13:23:46'),
+	('5', '104', '1', '0', '1', '2020-01-08 21:00:29'),
+	('6', '101', '2', '0', '0', '2020-01-08 21:03:13'),
+	('7', '105', '2', '0', '1', '2020-01-08 21:20:29'),
+	('8', '102', '1', '0', '0', '2020-01-09 23:54:33'),
+	('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+	('10', '104', '1', '0', '0', '2020-01-11 18:34:49'),
+	('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
 
 
 DROP TABLE IF EXISTS runner_orders;
 CREATE TABLE runner_orders (
-  "order_id" INTEGER,
-  "runner_id" INTEGER,
-  "pickup_time" TIMESTAMP,
-  "distance" FLOAT,
-  "duration" INTEGER,
-  "cancellation" VARCHAR(23)
+	"order_id" INTEGER,
+	"runner_id" INTEGER,
+	"pickup_time" TIMESTAMP,
+	"distance" FLOAT,
+	"duration" INTEGER,
+	"cancellation" VARCHAR(23)
 );
 
 INSERT INTO runner_orders
-  ("order_id", "runner_id", "pickup_time", "distance", "duration", "cancellation")
+	("order_id", "runner_id", "pickup_time", "distance", "duration", "cancellation")
 VALUES
-  ('1', '1', '2020-01-01 18:15:34', '20', '32', NULL),
-  ('2', '1', '2020-01-01 19:10:54', '20', '27', NULL),
-  ('3', '1', '2020-01-03 00:12:37', '13.4', '20', NULL),
-  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
-  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
-  ('6', '3', NULL, '0', '0', 'Restaurant Cancellation'),
-  ('7', '2', '2020-01-08 21:30:45', '25', '25', NULL),
-  ('8', '2', '2020-01-10 00:15:02', '23.4', '15', NULL),
-  ('9', '2', NULL, '0', '0', 'Customer Cancellation'),
-  ('10', '1', '2020-01-11 18:50:20', '10', '10', NULL);
+	('1', '1', '2020-01-01 18:15:34', '20', '32', NULL),
+	('2', '1', '2020-01-01 19:10:54', '20', '27', NULL),
+	('3', '1', '2020-01-03 00:12:37', '13.4', '20', NULL),
+	('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+	('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+	('6', '3', NULL, '0', '0', 'Restaurant Cancellation'),
+	('7', '2', '2020-01-08 21:30:45', '25', '25', NULL),
+	('8', '2', '2020-01-10 00:15:02', '23.4', '15', NULL),
+	('9', '2', NULL, '0', '0', 'Customer Cancellation'),
+	('10', '1', '2020-01-11 18:50:20', '10', '10', NULL);
 
 
 DROP TABLE IF EXISTS pizza_names;
 CREATE TABLE pizza_names (
-  "pizza_id" INTEGER,
-  "pizza_name" TEXT
+	"pizza_id" INTEGER,
+	"pizza_name" TEXT
 );
 INSERT INTO pizza_names
-  ("pizza_id", "pizza_name")
+	("pizza_id", "pizza_name")
 VALUES
-  (1, 'Meatlovers'),
-  (2, 'Vegetarian');
+	(1, 'Meatlovers'),
+	(2, 'Vegetarian');
 
 
 DROP TABLE IF EXISTS pizza_recipes;
 CREATE TABLE pizza_recipes (
-  "pizza_id" INTEGER,
-  "toppings" TEXT
+	"pizza_id" INTEGER,
+	"toppings" TEXT
 );
 INSERT INTO pizza_recipes
-  ("pizza_id", "toppings")
+	("pizza_id", "toppings")
 VALUES
-  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
-  (2, '4, 6, 7, 9, 11, 12');
+	(1, '1, 2, 3, 4, 5, 6, 8, 10'),
+	(2, '4, 6, 7, 9, 11, 12');
 
 
 DROP TABLE IF EXISTS pizza_toppings;
 CREATE TABLE pizza_toppings (
-  "topping_id" INTEGER,
-  "topping_name" TEXT
+	"topping_id" INTEGER,
+	"topping_name" TEXT
 );
 INSERT INTO pizza_toppings
-  ("topping_id", "topping_name")
+	("topping_id", "topping_name")
 VALUES
-  (1, 'Bacon'),
-  (2, 'BBQ Sauce'),
-  (3, 'Beef'),
-  (4, 'Cheese'),
-  (5, 'Chicken'),
-  (6, 'Mushrooms'),
-  (7, 'Onions'),
-  (8, 'Pepperoni'),
-  (9, 'Peppers'),
-  (10, 'Salami'),
-  (11, 'Tomatoes'),
-  (12, 'Tomato Sauce');
+  	(1, 'Bacon'),
+	(2, 'BBQ Sauce'),
+	(3, 'Beef'),
+	(4, 'Cheese'),
+	(5, 'Chicken'),
+	(6, 'Mushrooms'),
+	(7, 'Onions'),
+	(8, 'Pepperoni'),
+	(9, 'Peppers'),
+	(10, 'Salami'),
+	(11, 'Tomatoes'),
+	(12, 'Tomato Sauce');
 
 -- A. Pizza Metrics
 -- 1. How many pizzas were ordered?
 
 SELECT
-  COUNT(customer_orders.order_id) AS orders
+	COUNT(customer_orders.order_id) AS orders
 FROM pizza_runner.customer_orders;
 
 
@@ -146,8 +146,8 @@ ORDER BY runner_orders.runner_id ASC;
 
 SELECT
 	customer_orders.pizza_id,
-  pizza_names.pizza_name,
-  COUNT(customer_orders.pizza_id) AS total_orders
+	pizza_names.pizza_name,
+	COUNT(customer_orders.pizza_id) AS total_orders
 FROM pizza_runner.customer_orders
 INNER JOIN pizza_runner.pizza_names
 	ON customer_orders.pizza_id = pizza_names.pizza_id
@@ -175,8 +175,8 @@ ORDER BY customer_orders.customer_id ASC;
 WITH pizza_cte AS
 (
   SELECT
-  	customer_orders.order_id,
-  	COUNT(customer_orders.order_id) as total
+	customer_orders.order_id,
+	COUNT(customer_orders.order_id) as total
   FROM pizza_runner.customer_orders
   INNER JOIN pizza_runner.runner_orders
   	ON customer_orders.order_id = runner_orders.order_id
@@ -194,22 +194,22 @@ FROM pizza_cte;
 
 SELECT
 	customer_orders.customer_id,
-  SUM
-  (
-    CASE
-      WHEN customer_orders.exclusions != '0' OR customer_orders.extras != '0'
-      THEN 1
-      ELSE 0
-    END
-  ) AS with_changes,
-  SUM
-  (
-    CASE
-      WHEN customer_orders.exclusions = '0' AND customer_orders.extras = '0'
-      THEN 1
-      ELSE 0
-    END
-  ) AS no_changes
+	SUM
+	(
+	CASE
+		WHEN customer_orders.exclusions != '0' OR customer_orders.extras != '0'
+		THEN 1
+		ELSE 0
+	END
+	) AS with_changes,
+	SUM
+	(
+	CASE
+		WHEN customer_orders.exclusions = '0' AND customer_orders.extras = '0'
+		THEN 1
+		ELSE 0
+	END
+	) AS no_changes
 FROM pizza_runner.customer_orders
 INNER JOIN pizza_runner.runner_orders
 	ON customer_orders.order_id = runner_orders.order_id
@@ -225,14 +225,14 @@ WITH pizza_exclusions_extras_cte AS
 (
 SELECT
 	customer_orders.customer_id,
-  SUM
-  (
-    CASE
-      WHEN customer_orders.exclusions != '0' AND customer_orders.extras != '0'
-      THEN 1
-      ELSE 0
-    END
-  ) AS count_exlcusions_extras
+	SUM
+	(
+	CASE
+		WHEN customer_orders.exclusions != '0' AND customer_orders.extras != '0'
+		THEN 1
+		ELSE 0
+	END
+	) AS count_exlcusions_extras
 FROM pizza_runner.customer_orders
 INNER JOIN pizza_runner.runner_orders
 	ON customer_orders.order_id = runner_orders.order_id
@@ -262,7 +262,7 @@ ORDER BY hour ASC;
 
 SELECT 
 	to_char(customer_orders.order_time, 'Day') AS day_of_week, 
-  COUNT(customer_orders.order_id) AS pizza_ordered
+	COUNT(customer_orders.order_id) AS pizza_ordered
 FROM pizza_runner.customer_orders
 GROUP BY day_of_week
 ORDER BY pizza_ordered DESC;
